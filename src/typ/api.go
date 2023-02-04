bad6720742b4a119d7f0c9aeeacf4bf4234511c5
xiangqians <shixiangqian.s@gmail.com>
2023-02-04 14:13:16 +0800

    定义typ

---
+++ src/typ/api.go
@@ -0,0 +1,44 @@
+// api type
+// @author xiangqian
+// @date 14:07 2023/02/04
+package typ
+
+import "encoding/gob"
+
+// Abs 抽象实体定义
+type Abs struct {
+	Id      int64  `form:"id" binding:"gte=0"`    // 主键id
+	Rem     string `form:"rem" binding:"max=200"` // 备注
+	Del     byte   `form:"del"`                   // 删除标识，0-正常，1-删除
+	AddTime int64  `form:"addTime"`               // 创建时间（时间戳，s）
+	UpdTime int64  `form:"updTime"`               // 修改时间（时间戳，s）
+}
+
+// User 用户
+type User struct {
+	Abs
+	Name     string `form:"name" binding:"required,excludes= ,min=1,max=60"`               // 用户名
+	Nickname string `form:"nickname"binding:"max=60"`                                      // 昵称
+	Passwd   string `form:"passwd" binding:"required,excludes= ,max=100"`                  // 密码
+	RePasswd string `form:"rePasswd" binding:"required,excludes= ,max=100,eqfield=Passwd"` // retype Passwd
+}
+
+// File 文件
+type File struct {
+	Abs
+	Name string `form:"name" binding:"required,min=1,max=60"` // 文件名称
+}
+
+// Dir 文件
+type Dir struct {
+	Abs
+	ParentId int64  `form:"parentId" binding:"gte=0"`             // 父目录id
+	Name     string `form:"name" binding:"required,min=1,max=60"` // 目录名称
+}
+
+// 注册模型
+func init() {
+	gob.Register(User{})
+	gob.Register(File{})
+	gob.Register(Dir{})
+}
---
+++ src/typ/locale.go
@@ -0,0 +1,9 @@
+// locale
+// @author xiangqian
+// @date 13:13 2023/02/04
+package typ
+
+const (
+	LocaleZh = "zh"
+	LocaleEn = "en"
+)
