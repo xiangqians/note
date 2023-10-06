434716b77f738c8f583344cf523f50c127ee0cc4
xiangqians <shixiangqian.s@gmail.com>
2023-10-06 12:35:53 +0800

    新增图片、音视频数据表

--- data/id/database.sql
+++ data/id/database.sql
@@ -4,15 +4,15 @@
 DROP TABLE IF EXISTS `image`;
 CREATE TABLE `image` -- 图片信息表
 (
-    `id`        INTEGER PRIMARY KEY AUTOINCREMENT, -- id
-    `name`      VARCHAR(64) DEFAULT '',            -- 名称
-    `type`      VARCHAR(8)  DEFAULT '',            -- 类型
-    `size`      INTEGER     DEFAULT 0,             -- 大小，单位：byte
-    `hist`      TEXT        DEFAULT '',            -- history（历史记录）
-    `hist_size` INTEGER     DEFAULT 0,             -- history（历史记录）大小，单位：byte
-    `del`       TINYINT     DEFAULT 0,             -- 删除标识，0-正常，1-删除
-    `add_time`  INTEGER     DEFAULT 0,             -- 创建时间（时间戳，s）
-    `upd_time`  INTEGER     DEFAULT 0              -- 修改时间（时间戳，s）
+    `id`           INTEGER PRIMARY KEY AUTOINCREMENT, -- id
+    `name`         VARCHAR(64) DEFAULT '',            -- 名称
+    `type`         VARCHAR(8)  DEFAULT '',            -- 类型
+    `size`         INTEGER     DEFAULT 0,             -- 大小，单位：byte
+    `history`      TEXT        DEFAULT '',            -- 历史
+    `history_size` INTEGER     DEFAULT 0,             -- 历史大小，单位：byte
+    `del`          TINYINT     DEFAULT 0,             -- 删除标识，0-正常，1-删除，2-永久删除
+    `add_time`     INTEGER     DEFAULT 0,             -- 创建时间（时间戳，s）
+    `upd_time`     INTEGER     DEFAULT 0              -- 修改时间（时间戳，s）
 );
 
 
@@ -22,15 +22,15 @@ CREATE TABLE `image` -- 图片信息表
 DROP TABLE IF EXISTS `audio`;
 CREATE TABLE `audio` -- 音频信息表
 (
-    `id`        INTEGER PRIMARY KEY AUTOINCREMENT, -- id
-    `name`      VARCHAR(64) DEFAULT '',            -- 名称
-    `type`      VARCHAR(8)  DEFAULT '',            -- 类型
-    `size`      INTEGER     DEFAULT 0,             -- 大小，单位：byte
-    `hist`      TEXT        DEFAULT '',            -- history（历史记录）
-    `hist_size` INTEGER     DEFAULT 0,             -- history（历史记录）大小，单位：byte
-    `del`       TINYINT     DEFAULT 0,             -- 删除标识，0-正常，1-删除
-    `add_time`  INTEGER     DEFAULT 0,             -- 创建时间（时间戳，s）
-    `upd_time`  INTEGER     DEFAULT 0              -- 修改时间（时间戳，s）
+    `id`           INTEGER PRIMARY KEY AUTOINCREMENT, -- id
+    `name`         VARCHAR(64) DEFAULT '',            -- 名称
+    `type`         VARCHAR(8)  DEFAULT '',            -- 类型
+    `size`         INTEGER     DEFAULT 0,             -- 大小，单位：byte
+    `history`      TEXT        DEFAULT '',            -- 历史
+    `history_size` INTEGER     DEFAULT 0,             -- 历史大小，单位：byte
+    `del`          TINYINT     DEFAULT 0,             -- 删除标识，0-正常，1-删除，2-永久删除
+    `add_time`     INTEGER     DEFAULT 0,             -- 创建时间（时间戳，s）
+    `upd_time`     INTEGER     DEFAULT 0              -- 修改时间（时间戳，s）
 );
 
 
@@ -40,15 +40,15 @@ CREATE TABLE `audio` -- 音频信息表
 DROP TABLE IF EXISTS `video`;
 CREATE TABLE `video` -- 视频信息表
 (
-    `id`        INTEGER PRIMARY KEY AUTOINCREMENT, -- id
-    `name`      VARCHAR(64) DEFAULT '',            -- 名称
-    `type`      VARCHAR(8)  DEFAULT '',            -- 类型
-    `size`      INTEGER     DEFAULT 0,             -- 大小，单位：byte
-    `hist`      TEXT        DEFAULT '',            -- history（历史记录）
-    `hist_size` INTEGER     DEFAULT 0,             -- history（历史记录）大小，单位：byte
-    `del`       TINYINT     DEFAULT 0,             -- 删除标识，0-正常，1-删除
-    `add_time`  INTEGER     DEFAULT 0,             -- 创建时间（时间戳，s）
-    `upd_time`  INTEGER     DEFAULT 0              -- 修改时间（时间戳，s）
+    `id`           INTEGER PRIMARY KEY AUTOINCREMENT, -- id
+    `name`         VARCHAR(64) DEFAULT '',            -- 名称
+    `type`         VARCHAR(8)  DEFAULT '',            -- 类型
+    `size`         INTEGER     DEFAULT 0,             -- 大小，单位：byte
+    `history`      TEXT        DEFAULT '',            -- 历史
+    `history_size` INTEGER     DEFAULT 0,             -- 历史大小，单位：byte
+    `del`          TINYINT     DEFAULT 0,             -- 删除标识，0-正常，1-删除，2-永久删除
+    `add_time`     INTEGER     DEFAULT 0,             -- 创建时间（时间戳，s）
+    `upd_time`     INTEGER     DEFAULT 0              -- 修改时间（时间戳，s）
 );
 
 
@@ -58,16 +58,15 @@ CREATE TABLE `video` -- 视频信息表
 DROP TABLE IF EXISTS `note`;
 CREATE TABLE `note` -- 笔记信息表
 (
-    `id`        INTEGER PRIMARY KEY AUTOINCREMENT, -- id
-    `pid`       INTEGER     DEFAULT 0,             -- 父id
-    `name`      VARCHAR(64) DEFAULT '',            -- 笔记名称
-    `type`      VARCHAR(8)  DEFAULT '',            -- 笔记类型
-    `size`      INTEGER     DEFAULT 0,             -- 笔记大小，单位：byte
-    `hist`      TEXT        DEFAULT '',            -- history（历史记录）
-    `hist_size` INTEGER     DEFAULT 0,             -- history（历史记录）笔记大小，单位：byte
-    `del`       TINYINT     DEFAULT 0,             -- 删除标识，0-正常，1-删除
-    `add_time`  INTEGER     DEFAULT 0,             -- 创建时间（时间戳，s）
-    `upd_time`  INTEGER     DEFAULT 0              -- 修改时间（时间戳，s）
+    `id`           INTEGER PRIMARY KEY AUTOINCREMENT, -- id
+    `pid`          INTEGER     DEFAULT 0,             -- 父id
+    `name`         VARCHAR(64) DEFAULT '',            -- 名称
+    `type`         VARCHAR(8)  DEFAULT '',            -- 类型
+    `size`         INTEGER     DEFAULT 0,             -- 大小，单位：byte
+    `history`      TEXT        DEFAULT '',            -- 历史
+    `history_size` INTEGER     DEFAULT 0,             -- 历史大小，单位：byte
+    `del`          TINYINT     DEFAULT 0,             -- 删除标识，0-正常，1-删除，2-永久删除
+    `add_time`     INTEGER     DEFAULT 0,             -- 创建时间（时间戳，s）
+    `upd_time`     INTEGER     DEFAULT 0              -- 修改时间（时间戳，s）
 );
--- 存储dir、md、pdf、html、zip等
-
+-- 存储dir、md、doc、pdf、html、zip等
--- src/typ/user.go
+++ src/typ/user.go
@@ -10,9 +10,9 @@ type User struct {
 	Abs
 	Name       string `json:"name" form:"name" binding:"required,excludes= ,min=1,max=32"`                   // 用户名
 	Nickname   string `json:"nickname" form:"nickname" binding:"max=60"`                                     // 昵称
-	OrigPasswd string `json:"origPasswd" form:"origPasswd"`                                                  // Original Password
+	OrigPasswd string `json:"origPasswd" form:"origPasswd"`                                                  // Original Password，原密码
 	Passwd     string `json:"passwd" form:"passwd" binding:"required,excludes= ,max=120"`                    // 密码
-	RePasswd   string `json:"rePasswd" form:"rePasswd" binding:"required,excludes= ,max=120,eqfield=Passwd"` // retype Passwd
+	RePasswd   string `json:"rePasswd" form:"rePasswd" binding:"required,excludes= ,max=120,eqfield=Passwd"` // retype Passwd，再次输入密码
 	Try        byte   `json:"try" form:"try"`                                                                // 尝试输入次数
 }
 
