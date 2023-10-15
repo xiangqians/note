7957d86213886f53635dc2058b9502423101d708
xiangqians <shixiangqian.s@gmail.com>
2023-10-15 23:40:42 +0800

    table右键菜单

--- res/static/js/table.js
+++ res/static/js/table.js
@@ -4,7 +4,60 @@
  */
 ;
 (function () {
-    return;
+
+    let $body = $($('body')[0])
+
+    // 右键菜单列表
+    let $ul = $('<ul class="menu"></ul>')
+    $ul.append($('<li>111</li>'))
+    $ul.append($('<li>222</li>'))
+    $ul.append($('<li>333</li>'))
+    $body.prepend($ul)
+
+    // 为tr添加右键菜单事件
+    let $trs = $('table tbody tr[name!="noData"]')
+    for (let i = 0, len = $trs.length; i < len; i++) {
+        let $tr = $($trs[i])
+        $tr.contextmenu(function (e) {
+            // 阻止默认行为
+            e.preventDefault()
+
+            // 设置菜单位置
+            let x = e.clientX
+            let y = e.clientY
+            if (x >= document.documentElement.clientWidth - $ul.offsetWidth) {
+                x = document.documentElement.clientWidth - $ul.offsetWidth
+            }
+            if (y >= document.documentElement.clientHeight - $ul.offsetHeight) {
+                y = document.documentElement.clientHeight - $ul.offsetHeight
+            }
+            $ul.css('left', `${x}px`)
+            $ul.css('top', `${y}px`)
+
+            // 显示菜单
+            $ul.css('display', 'block')
+
+        });
+    }
+
+    // 监听 html->body 鼠标点击事件
+    $body.click(function (e) {
+        // 隐藏菜单
+        $ul.css('display', 'none')
+    })
+
+    $ul.click(function (e) {
+        let tagName = e.target.tagName.toLowerCase()
+        if (tagName !== 'li') {
+            return
+        }
+        
+        let $li = $(e.target)
+        console.log($li)
+    })
+
+    return
+
     document.addEventListener("contextmenu", function (evt) {
         evt.preventDefault()
         list.style.display = "block"
@@ -29,13 +82,4 @@
     }
 })();
 
-// body添加右键菜单
-function bodyMenu($ul) {
-    let $body = $($('body')[0])
-    $body.contextmenu(function (event) {
-        alert("处理程序.contextmenu()被调用。");
-    });
-}
 
-function tableBodyMenu() {
-}
