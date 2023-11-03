b908530fa2ee6aeff2891980ed111dec6d5b7cad
xiangqians <shixiangqian.s@gmail.com>
2023-11-04 00:50:50 +0800

    上一次登录记录

--- res/db/database.sql
+++ res/db/database.sql
@@ -4,16 +4,19 @@
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` -- 用户信息表
 (
-    `id`             INTEGER PRIMARY KEY AUTOINCREMENT, -- 用户id
-    `name`           VARCHAR(64)  NOT NULL,             -- 用户名
-    `nickname`       VARCHAR(64)  DEFAULT '',           -- 昵称
-    `passwd`         VARCHAR(128) NOT NULL,             -- 密码
-    `rem`            VARCHAR(256) DEFAULT '',           -- 备注
-    `try`            TINYINT      DEFAULT 0,            -- 尝试输入密码次数，超过3次账号将会被锁定
-    `sign_in_record` TEXT         DEFAULT '',           -- 登录记录
-    `del`            TINYINT      DEFAULT 0,            -- 删除标识，0-正常，1-删除
-    `add_time`       INTEGER      DEFAULT 0,            -- 创建时间（时间戳，单位s）
-    `upd_time`       INTEGER      DEFAULT 0             -- 修改时间（时间戳，单位s），也可作锁定时间
+    `id`                   INTEGER PRIMARY KEY AUTOINCREMENT, -- 用户id
+    `name`                 VARCHAR(64)  NOT NULL,             -- 用户名
+    `nickname`             VARCHAR(64)  DEFAULT '',           -- 昵称
+    `passwd`               VARCHAR(128) NOT NULL,             -- 密码
+    `rem`                  VARCHAR(256) DEFAULT '',           -- 备注
+    `try`                  TINYINT      DEFAULT 0,            -- 尝试输入密码次数，超过3次账号将会被锁定
+    `last_sign_in_ip`      VARCHAR(64)  DEFAULT '',           -- 上一次登录IP
+    `last_sign_in_time`    INTEGER      DEFAULT 0,            -- 上一次登录时间（时间戳，单位s）
+    `current_sign_in_ip`   VARCHAR(64)  DEFAULT '',           -- 当前次登录IP
+    `current_sign_in_time` INTEGER      DEFAULT 0,            -- 当前登录时间（时间戳，单位s）
+    `del`                  TINYINT      DEFAULT 0,            -- 删除标识，0-正常，1-删除
+    `add_time`             INTEGER      DEFAULT 0,            -- 创建时间（时间戳，单位s）
+    `upd_time`             INTEGER      DEFAULT 0             -- 修改时间（时间戳，单位s），也可作锁定时间
 );
 
 
--- src/model/user.go
+++ src/model/user.go
@@ -7,19 +7,14 @@ import "encoding/gob"
 // User 用户
 type User struct {
 	Abs
-	Name         string `json:"name" form:"name" binding:"required,excludes= ,min=1,max=60"` // 用户名
-	Nickname     string `json:"nickname" form:"nickname" binding:"max=60"`                   // 昵称
-	Passwd       string `json:"passwd" form:"passwd" binding:"required,excludes= ,max=120"`  // 密码
-	Try          byte   `json:"try" form:"try"`                                              // 尝试输入次数
-	SignInRecord string `json:"sign_in_record" form:"sign_in_record"`                        // 登录记录
-}
-
-// SignInRecord 登录记录
-type SignInRecord struct {
-	LastSignInIp      string // 上一次登录IP
-	LastSignInTime    int64  // 上一次登录时间（时间戳，单位s）
-	CurrentSignInIp   string // 当前次登录IP
-	CurrentSignInTime int64  // 当前登录时间（时间戳，单位s）
+	Name              string `json:"name" form:"name" binding:"required,excludes= ,min=1,max=60"` // 用户名
+	Nickname          string `json:"nickname" form:"nickname" binding:"max=60"`                   // 昵称
+	Passwd            string `json:"passwd" form:"passwd" binding:"required,excludes= ,max=120"`  // 密码
+	Try               byte   `json:"try" form:"try"`                                              // 尝试输入次数
+	LastSignInIp      string `json:"last_sign_in_ip" form:"last_sign_in_ip"`                      // 上一次登录IP
+	LastSignInTime    int64  `json:"last_sign_in_time" form:"last_sign_in_time"`                  // 上一次登录时间（时间戳，单位s）
+	CurrentSignInIp   string `json:"current_sign_in_ip" form:"current_sign_in_ip"`                // 当前次登录IP
+	CurrentSignInTime int64  `json:"current_sign_in_time" form:"current_sign_in_time"`            // 当前登录时间（时间戳，单位s）
 }
 
 // AddUser 新增用户
