233d36d224f2d9985e92dbd98dcafe797d29bc11
xiangqians <shixiangqian.s@gmail.com>
2023-11-15 21:30:50 +0800

    Windows环境build.bat脚本

--- build.bat
+++ build.bat
@@ -1,27 +1,33 @@
 @echo off
-setlocal enabledelayedexpansion
 
 REM 当前目录
 set "CURRENT_DIR=%~dp0"
-echo Current Dir: %CURRENT_DIR%
 
 REM 输出目录
 set "OUTPUT_DIR=%CURRENT_DIR%build"
-echo Output  Dir: %OUTPUT_DIR%
+echo Output Dir: %OUTPUT_DIR%
 
+REM 清空输出目录
 REM 判断输出目录是否已存在，如果已存在则删除
-goto REM_DEL_OUTPUT_DIR
-REM 删除输出目录
-REM 删除一个目录：RMDIR [/S] [/Q] [drive:]path 或者 RD [/S] [/Q] [drive:]path
-REM /S 表示除目录本身外，还将删除指定目录下的所有子目录和文件，用于删除目录树。
-REM /Q 安静模式，带 /S 删除目录树时不要求确认。
-:REM_DEL_OUTPUT_DIR
 if exist %OUTPUT_DIR% (
+    goto REM_DEL_OUTPUT_DIR
+    REM 删除输出目录
+    REM 删除一个目录：RMDIR [/S] [/Q] [drive:]path 或者 RD [/S] [/Q] [drive:]path
+    REM /S 表示除目录本身外，还将删除指定目录下的所有子目录和文件，用于删除目录树。
+    REM /Q 安静模式，带 /S 删除目录树时不要求确认。
+    :REM_DEL_OUTPUT_DIR
     rd /s /q %OUTPUT_DIR%
 )
+REM 重新创建输出目录
+md "%OUTPUT_DIR%"
 
 REM 拷贝资源
-set COPY_RES=db i18n static template note.ini
+REM 拷贝note.ini文件
+copy "res\note.ini" "%OUTPUT_DIR%" > nul
+REM 拷贝db文件
+md "%OUTPUT_DIR%\db"
+copy "res\db\database.db" "%OUTPUT_DIR%\db" > nul
+REM 拷贝data目录
 goto R
 REM Xcopy命令格式：XCOPY source [destination]
 REM 参数介绍：
@@ -36,52 +42,23 @@ REM /Q 复制时不显示文件名
 REM /T 创建目录结构，但不复制文件。不包括空目录或子目录。/T /E 包括空目录和子目录
 REM /Y 禁止提示以确认改写一个现存目标文件
 :R
-for %%i in (%COPY_RES%) do (
-    echo "%%i" | findstr "\." >nul && (
-        REM 拷贝文件
-        copy "res\%%i" "%OUTPUT_DIR%" > nul
-    ) || (
-        REM 拷贝目录
-        xcopy "res\%%i" "%OUTPUT_DIR%\%%i" /s /e /h /i /y /f > nul
-    )
-)
+xcopy "res\data" "%OUTPUT_DIR%\data" /s /e /h /i /y /f > nul
 
-exit
+REM 包名称
+for /F %%i in ('go env GOOS') do (set OS=%%i)
+for /F %%i in ('go env GOARCH') do (set ARCH=%%i)
+set "PKG_NAME=auto_deploy_%OS%_%ARCH%.exe"
 
+REM 包路径
+set "PKG_PATH=%OUTPUT_DIR%\%PKG_NAME%"
 
-REM copy
-xcopy data "%outputDir%/data" /s /e /h /i /y
-echo "cp data"
-xcopy i18n "%outputDir%/i18n" /s /e /h /i /y
-echo "cp i18n"
-xcopy static "%outputDir%/static" /s /e /h /i /y
-echo "cp static"
-xcopy template "%outputDir%/template" /s /e /h /i /y
-echo "cp template"
-
-
-
-
-REM pkgName
-for /F %%i in ('go env GOOS') do (set os=%%i)
-for /F %%i in ('go env GOARCH') do (set arch=%%i)
-set pkgName=auto_deploy_%os%_%arch%.exe
-echo pkgName: %pkgName%
-echo
-
-REM pkgPath & build
-set pkgPath="%outputDir%/%pkgName%"
-cd ./src && go build -ldflags="-s -w" -o %pkgPath%
-::cd ./src && go build -ldflags="-s -w" -o %pkgPath% && upx -9 --brute %pkgPath%
-echo pkgPath: %pkgPath%
-echo
+REM 构建
+cd ./src && go build -ldflags="-s -w" -o %PKG_PATH%
+::cd ./src && go build -ldflags="-s -w" -o %PKG_PATH% && upx -9 --brute %pkgPath%
 
 REM startup.bat
-set startupPath=%outputDir%/startup.bat
-echo REM startup.bat > %startupPath%
-echo %pkgName% >> %startupPath%
-echo pause >> %startupPath%
-echo startupPath: %startupPath%
-echo
+set "STARTUP_PATH=%OUTPUT_DIR%\startup.bat"
+echo %PKG_NAME% > %STARTUP_PATH%
+echo pause >> %STARTUP_PATH%
 
 pause
--- src/main.go
+++ src/main.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"embed"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,7 +12,19 @@ import (
 	"note/src/model"
 )
 
+//go:embed res/static
+var staticFiles embed.FS
+
 func main() {
+	files, err := staticFiles.ReadDir("res/static/css")
+	if err != nil {
+		panic(err)
+	}
+	for _, file := range files {
+		fileName := file.Name()
+		log.Println("===>", fileName)
+	}
+
 	// 创建了一个 http.ServeMux 对象，用于注册和管理路由和处理器函数
 	mux := http.NewServeMux()
 
@@ -19,7 +32,7 @@ func main() {
 	handler.Handle(mux)
 
 	port := model.Ini.Server.Port
-	
+
 	server := &http.Server{
 		Addr:    fmt.Sprintf(":%d", port),
 		Handler: mux,
