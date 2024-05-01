b4eb492b9d4032684041f0c18f8b716fd9f80b42
xiangqians <shixiangqian.s@gmail.com>
2024-05-01 17:40:09 +0800

    动态传入数据源

--- startup.sh
+++ startup.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+#java -Dfile.encoding=UTF-8 -Xss4096K -Xms1G -Xmx1G -jar note-prod-2024.04.jar -DNOTE_DATASOURCE=C:\Users\xiangqian\Desktop\tmp\note\data\database.db >/dev/null 2>&1 &
 java -Dfile.encoding=UTF-8 -Xss4096K -Xms1G -Xmx1G -jar note-prod-2024.04.jar >/dev/null 2>&1 &
